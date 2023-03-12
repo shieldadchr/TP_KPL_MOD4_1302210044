4f3834097b511c109583ae60956a5f228bd4b386
shieldadchr <shielda.diasavitri@gmail.com>
2023-03-12 22:42:23 +0700

    TPMOD4

---
+++ TP_MOD4_KPL_1302210044_RPW/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    enum kelurahan
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+
+   
+    private static int kodepos(kelurahan K)
+    {
+        int[] L =
+        {
+            40266,
+            40287,
+            40267,
+            40256,
+            40287,
+            40286,
+            40286,
+            40286,
+            40272,
+            40274,
+            40273
+
+
+
+        };
+        return L[(int)K];
+
+    }
